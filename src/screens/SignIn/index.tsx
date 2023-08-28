36bb3613ace9524b5d65a26136726cb2bd8c3aba
nykael <nykaelccardial@gmail.com>
2023-08-28 08:59:29 -0300

    Button

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,14 @@
 import { Container, Title, Slogan } from './styles';
 import backGroundImg from '../../assets/background.png'
+import { Button } from '../../components/Button';
 
 export function SignIn() {
   return (
     <Container source={backGroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
+
+      <Button title='Entrar com Google' />
     </Container>
   );
 }
